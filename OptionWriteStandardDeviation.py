e33d73a92f7093840f479092328b7569aa81a259
Ranjith <vkranjithkumar66@gmail.com>
2021-11-06 12:45:07 +0530

    Added excel support to standard deviation

--- OptionWriteStandardDeviation.py
+++ OptionWriteStandardDeviation.py
@@ -2,7 +2,8 @@ import yfinance as yf
 from pandas import Series
 from constants import stocks, indexSymbolForYfinance
 from math import log,exp
-from datetime import date
+from openpyxl import Workbook
+from openpyxl.styles import PatternFill
 from os import path ,remove
 
 
@@ -24,7 +25,27 @@ def getReturnsForData(closeSeries):
 
     return dailyReturnlist
 
-def computeSTDandAddtoFile(stock, data ,No_of_days_to_Expire, fp):
+def addHeaderToExcel(workSheet,expiry):
+    workSheet.append(['S.No',
+                      'Stock Name',
+                      'Current Price',
+                      'Daily Volatility',
+                      'Volatility after {} days'.format(expiry),
+                      '1 SD (LL)',
+                      '1 SD (HH)',
+                      '2 SD (LL)',
+                      '2 SD (HH)',
+                      ])
+    cellRange = workSheet['A1':'I1']
+    redFill = PatternFill(start_color='FFFFFF00',
+                          end_color='FFFFFF00',
+                          fill_type='solid')
+
+    for cell in cellRange[0]:
+        cell.fill = redFill
+
+
+def computeSTDandAddtoExcel(stock, data ,No_of_days_to_Expire, workSheet, sNo):
 
     # print(data.iloc[-1], data.__class__)
     # print(data[-9:])
@@ -48,32 +69,56 @@ def computeSTDandAddtoFile(stock, data ,No_of_days_to_Expire, fp):
     lowerLimit_2SD = (exp((mean_at_expiry - (2 * std_at_expiry))/100)) * data.iloc[-1]
     upperLimit_2SD = (exp((mean_at_expiry + (2 * std_at_expiry))/100)) * data.iloc[-1]
 
-    fp.write("******************************************************\n")
-    fp.write("Stock Name       : {}\n".format(stock))
-    fp.write("Current Price    : {}\n".format(data.iloc[-1]))
-    fp.write("Daily Volatility : {}\n".format(standardDeviation))
-    fp.write("Volatily after {} days: {}\n".format(No_of_days_to_Expire,std_at_expiry))
-    fp.write("Range for 1SD    : {} - {}\n".format(lowerLimit_1SD,upperLimit_1SD))
-    fp.write("Range for 2SD    : {} - {}\n".format(lowerLimit_2SD,upperLimit_2SD))
-    fp.write("******************************************************\n")
+    workSheet.append([sNo,
+                      stock,
+                      data.iloc[-1],
+                      standardDeviation,
+                      std_at_expiry,
+                      lowerLimit_1SD,
+                      upperLimit_1SD,
+                      lowerLimit_2SD,
+                      upperLimit_2SD])
+
+    # fp.write("******************************************************\n")
+    # fp.write("Stock Name       : {}\n".format(stock))
+    # fp.write("Current Price    : {}\n".format(data.iloc[-1]))
+    # fp.write("Daily Volatility : {}\n".format(standardDeviation))
+    # fp.write("Volatily after {} days: {}\n".format(No_of_days_to_Expire,std_at_expiry))
+    # fp.write("Range for 1SD    : {} - {}\n".format(lowerLimit_1SD,upperLimit_1SD))
+    # fp.write("Range for 2SD    : {} - {}\n".format(lowerLimit_2SD,upperLimit_2SD))
+    # fp.write("******************************************************\n")
 
 if __name__ == "__main__":
 
     No_of_days_to_Expire = int(input("Number of days to expire : "))
 
-    fileName = "./standardDeviation_data({} days)".format(No_of_days_to_Expire)
+    fileName = "./standardDeviation_data({} days).xlsx".format(No_of_days_to_Expire)
 
     if (path.exists(fileName) == True):
         remove(fileName)
 
-    with open(fileName,mode ='a') as fp:
-        for index in indexSymbolForYfinance:
-            data = yf.download(indexSymbolForYfinance[index], period="3y")["Close"]
-            computeSTDandAddtoFile(index, data,No_of_days_to_Expire, fp)
+    wb = Workbook()
+    ws = wb.active
 
-        for stock in stocks:
-            data = yf.download(stocks[stock]+".NS" , period="3y")["Close"]
-            computeSTDandAddtoFile(stock,data,No_of_days_to_Expire, fp)
+    addHeaderToExcel(ws,No_of_days_to_Expire)
+    count = 1
+
+    for index in indexSymbolForYfinance:
+        data = yf.download(indexSymbolForYfinance[index], period="3y")["Close"]
+        computeSTDandAddtoExcel(index, data,No_of_days_to_Expire, ws, count)
+        count+=1
+
+    for stock in stocks:
+        data = yf.download(stocks[stock]+".NS" , period="3y")["Close"]
+        computeSTDandAddtoExcel(stock,data,No_of_days_to_Expire, ws,count)
+        count += 1
+
+    wb.save(fileName)
+
+    wb.close()
+
+
+
             
             
 
