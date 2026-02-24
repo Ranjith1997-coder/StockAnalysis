aa34ac4d23075447a8615a685607d5ec8f512441
Ranjith <vkranjithkumar66@gmail.com>
2026-02-24 20:34:15 +0530

    initial commit

--- backtest/__init__.py
+++ backtest/__init__.py
@@ -3,5 +3,9 @@ Backtesting infrastructure for stock trading strategies
 """
 
 from backtest.backtest import Backtester, BacktestResult, Trade
+from backtest.optimizer import ThresholdOptimizer, BulkOptimizer, SEARCH_SPACES
 
-__all__ = ['Backtester', 'BacktestResult', 'Trade']
+__all__ = [
+    'Backtester', 'BacktestResult', 'Trade',
+    'ThresholdOptimizer', 'BulkOptimizer', 'SEARCH_SPACES',
+]
